d5c239b9f97a54e7e0bb4e0a4a8e31053efd97e8
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2023-03-25 20:23:22 +0100

    1st commit

---
+++ src/js/index_script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const navBar = document.querySelector(".navbar");
+
+// Function to handle the change of opacity when we hover on the links of the navbar
+const handlerover = function (event) {
+  if (event.target.classList.contains("navlink")) {
+    const link = event.target;
+    const siblings = link.closest(".navbar").querySelectorAll(".navlink");
+
+    // link.classList.toggle("border-bottom");
+
+    siblings.forEach((element) => {
+      if (element !== link) element.style.opacity = this;
+    });
+  } else {
+    document.querySelectorAll(".navlink").forEach((element) => (element.style.opacity = 1));
+  }
+};
+
+navBar.addEventListener("mouseover", handlerover.bind(0.5));
+navBar.addEventListener("mouseout", handlerover.bind(0.5));

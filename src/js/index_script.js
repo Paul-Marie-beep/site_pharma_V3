5f901992e595266d4f24686881e60bf315091d11
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2023-03-25 22:51:15 +0100

    point d'étape

--- src/js/index_script.js
+++ src/js/index_script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const navBar = document.querySelector(".navbar");
+const title = document.querySelector(".titre");
+const subTitle = document.querySelector(".titre__sous");
+const containerOne = document.querySelector(".");
 
 // Function to handle the change of opacity when we hover on the links of the navbar
 const handlerover = function (event) {
@@ -20,3 +23,16 @@ const handlerover = function (event) {
 
 navBar.addEventListener("mouseover", handlerover.bind(0.5));
 navBar.addEventListener("mouseout", handlerover.bind(0.5));
+
+// We hide the title and one the fade in animation is finisshed, we set the opacity to 1
+
+const fadeInHelper = function (div, time) {
+  const delay = time * 1000;
+  div.style.opacity = 0;
+  setTimeout(() => {
+    div.style.opacity = 1;
+  }, delay);
+};
+
+fadeInHelper(title, 1.2);
+fadeInHelper(subTitle, 1.5);

e2df172207bd894b0c8d17e2ecead9faf272ced1
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2023-03-29 12:09:57 +0200

    ajout navbar sticky

--- src/js/index_script.js
+++ src/js/index_script.js
@@ -1,16 +1,15 @@
 "use strict";
 
+const main = document.querySelector(".main");
+
 const navBar = document.querySelector(".navbar");
-const title = document.querySelector(".titre__sur");
-const subTitle = document.querySelector(".titre__sous");
+const navHeight = navBar.getBoundingClientRect().height;
 
 // Function to handle the change of opacity when we hover on the links of the navbar
 const handlerover = function (event) {
   if (event.target.classList.contains("navlink")) {
     const link = event.target;
     const siblings = link.closest(".navbar").querySelectorAll(".navlink");
-
-    // link.classList.toggle("border-bottom");
 
     siblings.forEach((element) => {
       if (element !== link) element.style.opacity = this;
@@ -22,3 +21,22 @@ const handlerover = function (event) {
 
 navBar.addEventListener("mouseover", handlerover.bind(0.5));
 navBar.addEventListener("mouseout", handlerover.bind(0.5));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Sticky nav
+const obsCallback = function (entries) {
+  const [entry] = entries;
+  console.log(entries, entry);
+  if (entry.isIntersecting) navBar.classList.add("nav-sticky");
+  else navBar.classList.remove("nav-sticky");
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.55,
+};
+
+const navObserver = new IntersectionObserver(obsCallback, obsOptions);
+navObserver.observe(main);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
